70102ab66e73bc49694155ea40a8bdd22c4b53d3
GlenPearce <glenpearce21@gmail.com>
2024-08-20 07:36:34 +0100

    Append
    Added the function to append the scene with the geometry nodes tools, materials and mesh ref (colonist). This is necessary to use the button to add vert colour by angle to a mesh and allows for easier implementation of any geometry node tools and resources in the future.

--- Blender/RovaTools/__init__.py
+++ Blender/RovaTools/__init__.py
@@ -1,239 +1,157 @@
 bl_info = {
-        "name": "Rova Tools",
-        "description": "Tools used to make Rova easier.",
-        "author": "Glen Pearce",
-        "version": (1, 0),
-        "blender": (4, 0, 0),
-        "location": "3D Viewport > Sidebar > RovaTools",
-        "warning": "", # used for warning icon and text in add-ons panel
-        "category": "Development"
-        }
+    "name": "Rova Tools",
+    "description": "Tools used to make Rova easier.",
+    "author": "Glen Pearce",
+    "version": (1, 0),
+    "blender": (4, 0, 0),
+    "location": "3D Viewport > Sidebar > RovaTools",
+    "warning": "",  # used for warning icon and text in add-ons panel
+    "category": "Development"
+}
 
 import bpy
-import random
 import os
 
 
 class PANEL_PT_Panel(bpy.types.Panel):
-    """
-    Layout of the panel
-    """
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "RovaTools"
+    bl_label = "RovaTools"
 
-    # where the panel gets added
-    bl_space_type = "VIEW_3D"  # gets shown in 3d viewport
-    bl_region_type = "UI"  # referencing sidebar region
-
-    # Label names
-    bl_category = "RovaTools"  # sidebar tab name
-    bl_label = "RovaTools"  # in the add-on, top of panel name
-
-    # Draw out buttons/layout, add more buttons and layout options here
     def draw(self, context):
-        """define the layout of the panel"""
-        layout  = self.layout
+        layout = self.layout
         layout.use_property_split = True
         layout.use_property_decorate = False
 
-        #Panel layout & buttons
         row = self.layout.row()
         row.operator("form.resource_import", text="Import Rova Resources")
+        row = self.layout.row()
+        row.label(text="Import first ^^^ just the once :)")
+        row = self.layout.row()
+        row.operator("form.add_geo_node_modifier", text="Add ColourByAngle Modifier")
         row = self.layout.row()
         row.label(text="Select faces in edit mode, then choose a colour below")
         row = self.layout.row()
         row.label(text="Vertex Painting Colours")
         row = self.layout.row()
-
-        #Add new colours in here
-        row.operator("form.paint_face", text="Red").color = (1,0,0,1)
-        row.operator("form.paint_face", text="Blue").color = (0,0,1,1)
+        row.operator("form.paint_face", text="Red").color = (1, 0, 0, 1)
+        row.operator("form.paint_face", text="Blue").color = (0, 0, 1, 1)
         row = self.layout.row()
-        row.operator("form.paint_face", text="Green").color = (0,1,0,1)
-        row.operator("form.paint_face", text="Yellow").color = (1,1,0,1)
+        row.operator("form.paint_face", text="Green").color = (0, 1, 0, 1)
+        row.operator("form.paint_face", text="Yellow").color = (1, 1, 0, 1)
         row = self.layout.row()
-        row.operator("form.paint_face", text="Cyan").color = (0,1,1,1)
-        row.operator("form.paint_face", text="Purple").color = (1,0,1,1)
-        row.operator("form.paint_face", text="Orange").color = (1,0.5,0,1)
+        row.operator("form.paint_face", text="Cyan").color = (0, 1, 1, 1)
+        row.operator("form.paint_face", text="Purple").color = (1, 0, 1, 1)
+        row.operator("form.paint_face", text="Orange").color = (1, 0.5, 0, 1)
 
 
 class MAT_OT_Paint(bpy.types.Operator):
-
-    """
-    Select faces on an object and quickly paint them, from a choice of colours
-    """
-
-    bl_idname = "form.paint_face"  # Unique identifier for buttons and menu items to reference.
-    bl_label = "Applies selected colour to selected faces "  # Display name in the interface.
-    bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
+    bl_idname = "form.paint_face"
+    bl_label = "Applies selected colour to selected faces "
+    bl_options = {'REGISTER', 'UNDO'}
 
     color: bpy.props.FloatVectorProperty(size=4)
 
-    def execute(self, context):  # execute() is called when running the operator.
-
+    def execute(self, context):
         VertColDataName = "VertColour"
 
-        #Checks for edit mode, single object, selection mode and that only 1 vertex data is available.
         if bpy.context.active_object.mode != 'EDIT':
-            self.report({'INFO'}, "Need an to be in edit mode.")
+            self.report({'INFO'}, "Need to be in edit mode.")
             return {'CANCELLED'}
 
         if not bpy.context.tool_settings.mesh_select_mode[2]:
-            self.report({'INFO'}, "Need an to be in face selection mode.")
+            self.report({'INFO'}, "Need to be in face selection mode.")
             return {'CANCELLED'}
 
         if len(bpy.context.selected_objects) != 1:
             self.report({'INFO'}, "Select One Object.")
             return {'CANCELLED'}
-    
+
         mesh = bpy.context.active_object.data
 
-        #create new layer if not one available
         if not mesh.vertex_colors:
-            vert_col = mesh.vertex_colors.new(name = VertColDataName)
+            vert_col = mesh.vertex_colors.new(name=VertColDataName)
             self.report({'INFO'}, "No Vertex Color data found, made new one.")
-        
-        #set the first color attribute as active, prevents creating an empty one when switching to vertex paint
-        mesh.attributes.active_color_index = 0
 
-        #set mode to vertex paint ready to paint face
+        mesh.attributes.active_color_index = 0
         bpy.ops.object.mode_set(mode='VERTEX_PAINT')
-        
-        #take selected face, paint each vert in face selection
         selected_faces = [face for face in mesh.polygons if face.select]
         for face in selected_faces:
             for loop_index in face.loop_indices:
-                # Set the color for each vertex of the face
                 mesh.vertex_colors.active.data[loop_index].color = self.color
 
         bpy.ops.object.mode_set(mode='EDIT')
-
 
         return {'FINISHED'}
 
 
 class FORM_OT_resource_import(bpy.types.Operator):
+    bl_idname = "form.resource_import"
+    bl_label = "Imports geometry node tool, texture atlas, debug material and colonist ref mesh"
+    bl_options = {'REGISTER', 'UNDO'}
 
+    def execute(self, context):
+        path = os.path.join(os.path.dirname(__file__), 'RovaResources', 'RovaResources.blend')
+        print("Blend file path:", path)
 
-    """
-    Imports any geometry node tools, debug material and models used for Rova
-    """
+        try:
+            with bpy.data.libraries.load(path) as (data_from, data_to):
+                # Filter to load only meshes, materials, and geometry nodes (node groups)
+                data_to.meshes = data_from.meshes
+                data_to.materials = data_from.materials
+                data_to.node_groups = data_from.node_groups
 
-    bl_idname = "form.resource_import"  # Unique identifier for buttons and menu items to reference.
-    bl_label = "Imports geometry node tool, texture atlas, debug material and colonist ref mesh "  # Display name in the interface.
-    bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
+            self.report({'INFO'}, "Imported successfully.")
 
+        except OSError as e:
+            self.report({'ERROR'}, f"Failed to load blend file: {e}")
+            return {'CANCELLED'}
 
-    def execute(self, context):  # execute() is called when running the operator.
-
-        #gets file path of the location the script is currently in, this is because the resource .blend file is in here too
-        path = os.path.join(os.path.dirname(__file__), 'RovaResources.blend')
-
-        with bpy.data.libraries.load(path) as (data_from, data_to):
-            for attr in dir(data_to):
-                setattr(data_to, attr, getattr(data_from, attr))
-        
         return {'FINISHED'}
 
 
-class FORM_OT_unity_export(bpy.types.Operator):
-    """
-    Export a house made in Blender, with the correct transforms to match Unity. Excludes children of empties that should contain assets already with these transforms.
-    """
-    bl_idname = "form.unity_export"  # Unique identifier for buttons and menu items to reference.
-    bl_label = "Applies transform values to match Unity and exports "  # Display name in the interface.
-    bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
+class FORM_OT_add_geo_node_modifier(bpy.types.Operator):
+    bl_idname = "form.add_geo_node_modifier"
+    bl_label = "Add ColourByAngle Modifier"
+    bl_options = {'REGISTER', 'UNDO'}
 
-    def execute(self, context):  # execute() is called when running the operator.
+    def execute(self, context):
+        obj = context.active_object
+        if not obj or obj.type != 'MESH':
+            self.report({'ERROR'}, "Active object is not a mesh.")
+            return {'CANCELLED'}
 
-        #store selected object and all children of object
-        obj = bpy.context.active_object
+        node_group_name = "ColourByAngleNode"
+        if node_group_name not in bpy.data.node_groups:
+            self.report({'ERROR'}, f"Node group '{node_group_name}' does not exist.")
+            return {'CANCELLED'}
 
-            #allows use of file path string
-        mytool = context.scene.my_tool
+        geo_node_modifier = obj.modifiers.new(name=node_group_name, type='NODES')
+        geo_node_modifier.node_group = bpy.data.node_groups[node_group_name]
 
-        # change the scale & rotation to match Unity and apply
-        obj.scale = (100, 100, 100)
-        obj.rotation_euler[0] = -1.5708
-
-        # Apply rotation and scale transforms to root object
-        bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
-
-        #select all child objects of root object with exceptions
-        #hides children of exluded names for empties, to keep their transforms
-        #ignores empties and applies transforms to empties unless they are called disc (lighting locations)
-        for child_object in obj.children_recursive:
-            for childchild_object in child_object.children_recursive:
-                if childchild_object.type == 'MESH' or 'Disc' in childchild_object.name:
-                    childchild_object.hide_set(True)
-                else:
-                    childchild_object.select_set(True)
-
-            else:
-                child_object.select_set(True)
-
-        child_objects = bpy.context.selected_objects
-        bpy.context.view_layer.objects.active = None  # Unset active object
-
-        #set child objects active and apply transforms
-        for child in child_objects:
-            bpy.context.view_layer.objects.active = child
-            bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
-            print(child.name + "Applied")
-            print("Transforms Applied: " + str(bpy.context.view_layer.objects.active))
-            if child.name == 'BackDoorNode':
-                bpy.context.active_object.rotation_euler = (0, 0, math.radians(180))
-
-            if 'Tag' in child_object.name and child_object.type != 'EMPTY':
-                        bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
-
-            bpy.context.view_layer.objects.active = None
-
-        bpy.context.view_layer.objects.active = obj
-
-        #Import FBX, file path is chosen by clicking the file icon on the panel of the add-on
-        bpy.ops.export_scene.fbx(filepath=mytool.filepath + obj.name + ".fbx", check_existing=True, filter_glob='*.fbx', use_selection=False,
-                                 use_visible=False, use_active_collection=False, global_scale=0.01,
-                                 apply_unit_scale=False, apply_scale_options='FBX_SCALE_ALL', use_space_transform=False,
-                                 bake_space_transform=False,
-                                 object_types={'ARMATURE', 'CAMERA', 'EMPTY', 'LIGHT', 'MESH', 'OTHER'},
-                                 use_mesh_modifiers=True, use_mesh_modifiers_render=True, mesh_smooth_type='OFF',
-                                 use_subsurf=False,
-                                 use_mesh_edges=False, use_tspace=False, use_triangles=False, use_custom_props=False,
-                                 add_leaf_bones=True, primary_bone_axis='Y', secondary_bone_axis='X',
-                                 use_armature_deform_only=False, armature_nodetype='NULL', bake_anim=True,
-                                 bake_anim_use_all_bones=True, bake_anim_use_nla_strips=True,
-                                 bake_anim_use_all_actions=True, bake_anim_force_startend_keying=True,
-                                 bake_anim_step=1.0, bake_anim_simplify_factor=1.0, path_mode='AUTO',
-                                 embed_textures=False, batch_mode='OFF', use_batch_own_dir=True, use_metadata=True,
-                                 axis_forward='-Z', axis_up='Y')
-
-        # Set the Unity transform values
-        obj.scale = (.01, .01, .01)
-        obj.rotation_euler[0] = 1.5708
-
-        bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
-
-
-
+        self.report({'INFO'}, f"Added '{node_group_name}' Geometry Node modifier.")
         return {'FINISHED'}
 
 
 classes = (
     PANEL_PT_Panel,
     MAT_OT_Paint,
-    FORM_OT_resource_import
+    FORM_OT_resource_import,
+    FORM_OT_add_geo_node_modifier
 )
 
 def register():
     from bpy.utils import register_class
     for cls in classes:
         register_class(cls)
-
+    print("Rova Tools registered successfully.")
 
 def unregister():
     from bpy.utils import unregister_class
     for cls in reversed(classes):
         unregister_class(cls)
-
+    print("Rova Tools unregistered successfully.")
 
 if __name__ == '__main__':
     register()
